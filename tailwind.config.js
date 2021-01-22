273369147852a2f5e09281d67e78d82da2feb5e1
gauthamkrishnax <gauthamkrishnax@gmail.com>
2021-01-22 15:22:40 +0530

    Initialised project Dokoto

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFF3CC",
+        secondary: "#FF9E8F"
+      },
+      fontFamily: {
+        ubuntu : "Ubuntu"
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
