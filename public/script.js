d8b898cdb926a8b21cecdddb5fb89e8b313b0ded
gauthamkrishnax <gauthamkrishnax@gmail.com>
2021-01-25 00:11:25 +0530

    Responsive design

--- public/script.js
+++ public/script.js
@@ -1,3 +1,11 @@
+const menu = document.querySelector("#menu");
+const sidebar = document.querySelector("#sidebar");
+
+
+menu.addEventListener("click", ()=>{
+    sidebar.classList.toggle("hidden")
+})
+
 
 // ADD NOTE POPUP
 
