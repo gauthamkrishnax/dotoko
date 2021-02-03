b27a77c4256ce48d542214ff417280734a3253ab
gauthamkrishnax <gauthamkrishnax@gmail.com>
2021-02-03 12:49:29 +0530

    beta build primary-front end

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   purge: {
     enabled: true,
-    content: ['./src/**/*.html',
-               './src/**/*.js'],
+    content: ['./public/**/*.html',
+               './public/**/*.js'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
